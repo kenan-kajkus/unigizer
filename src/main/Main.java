0f14c612fe35d5e757990912ec5f9e30a11feffb
kenan-kajkus <kenan@kajkus.eu>
2016-06-07 17:05:03 +0200

    GUI preparation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Application.launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
